b8344cf89a517f427c1dbf60ee35970dac0d0e38
Matias Casiba <casibagabriel@gmail.com>
2025-07-08 13:31:12 -0300

    Modificacion en el header de la página (descarto el carro de compras y la barra de buscador)

--- src/components/header/searchbar/Searchbar.jsx
+++ src/components/header/searchbar/Searchbar.jsx
@@ -1,12 +1,10 @@
-import { useContext } from 'react'
+
 import { Link } from 'react-router'
 import './Searchbar.scss'
-import CarritoContext from '../../../context/CarritoContext'
+
 
 
 const Searchbar = () => {
-
-  const {contarProductosCarritoContext} = useContext(CarritoContext)
 
   return (
     <>
@@ -14,23 +12,8 @@ const Searchbar = () => {
         <div className="search-bar__logo-container">
           <img className='search-bar__logo-img' src="/logo/me-logo.png" alt="logo me" />
         </div>
-
-        <form action="#" className='search-bar__form-container'>
-          <label htmlFor="busqueda" className='search-bar__form-label'>
-            <img className='search-bar__logo-search' src="/logo/logo-search.png" alt="logo del buscador" />
-          </label>
-          <input type="search" id="busqueda" className='search-bar__form-search' />
-          <button type='submit' className='search-bar__form-submit'>Buscar</button>
-        </form>
-
-        <div className="search-bar__carrito-container">
-          <Link to="/carrito" className='search-bar__cart-link'>
-            <img className='search-bar__cart-logo' src="/logo/cart-logo.webp" alt="logo carro" />
-            {contarProductosCarritoContext > 0 && (
-              <span className='search-bar__cart-count'>{contarProductosCarritoContext}</span>
-            )}
-          </Link>
-        </div>
+        <h1 className='search-bar__title'>LEDECO</h1>
+        
 
         <div className="menu-toogle">
           <label htmlFor="menu" className='menu-toogle__label'>
