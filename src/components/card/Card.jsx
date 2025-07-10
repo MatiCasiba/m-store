ae5efc4f208f86d4c919b7c338fc8129ad1fda6b
Matias Casiba <casibagabriel@gmail.com>
2025-07-10 19:27:16 -0300

    Modificación de las tarjetas (reemplazo la animación actual por otra y modifico tamaños)

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,14 +1,11 @@
 import './Card.scss';
 
 const Card = ({producto}) => {
-  
   return (
-    <>
-      <div className="card">
-        <article className='card__article'>
-          <div className="card__image-container">
-            <img className='card__image' src={producto.foto} alt={producto.nombre} />
-          </div>
+    <div className="card">
+      <article className='card__article'>
+        <div className="card__image-container">
+          <img className='card__image' src={producto.foto} alt={producto.nombre} />
           <div className="card__content">
             <h2 className='card__heading'>{producto.nombre}</h2>
             <p className='card__precios'>{producto.precio}</p>
@@ -16,9 +13,9 @@ const Card = ({producto}) => {
               <p className='card__text'>{producto.descripcion}</p>
             </div>
           </div>
-        </article>
-      </div>
-    </>
+        </div>
+      </article>
+    </div>
   )
 }
 
