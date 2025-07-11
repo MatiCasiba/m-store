2e710dca1559c6d451dd01952ef43c3c04a50f22
Matias Casiba <casibagabriel@gmail.com>
2025-07-10 21:12:45 -0300

    Agrego tipografía y diseño el precio a las tarjetas

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,10 +8,10 @@ const Card = ({producto}) => {
           <img className='card__image' src={producto.foto} alt={producto.nombre} />
           <div className="card__content">
             <h2 className='card__heading'>{producto.nombre}</h2>
-            <p className='card__precios'>{producto.precio}</p>
             <div className="card__description">
               <p className='card__text'>{producto.descripcion}</p>
             </div> {/* __description */}
+            <p className='card__precios'>{producto.precio}</p>
           </div> {/* __content */}
         </div> {/* __image-container */}
       </article>
