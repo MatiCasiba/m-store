45861487410292e756d15f74d3466cfab3e5794a
Matias Casiba <casibagabriel@gmail.com>
2025-07-11 14:58:37 -0300

    Solucion del menú toogle (ahora se desactiva cuando el usuario elige algún item)

--- src/components/header/NavItem.jsx
+++ src/components/header/NavItem.jsx
@@ -1,14 +1,20 @@
+
 import { NavLink } from 'react-router'
 
 const NavItem = ({item}) => {
+  const handleClick = () => {
+    const menuToggle = document.getElementById('menu')
+    if (menuToggle && menuToggle.checked) {
+      menuToggle.checked = false
+    }
+  }
+
   return (
-    <>
-      <li className='nav-bar__nav-item'>
-        <NavLink to={item.ruta} className="nav-bar__nav-link">
-          {item.nombre}
-        </NavLink>
-      </li>
-    </>
+    <li className='nav-bar__nav-item'>
+      <NavLink to={item.ruta} className="nav-bar__nav-link" onClick={handleClick}>
+        {item.nombre}
+      </NavLink>
+    </li>
   )
 }
 
